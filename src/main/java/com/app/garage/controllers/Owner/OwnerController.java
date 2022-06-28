321c120a982955efb56a4ec5142a17122ac6c31d
m7md <eng.2002.mohammad@gmail.com>
2022-06-28 13:17:30 +0300

    SLIDER ADDED

--- src/main/java/com/app/garage/controllers/Owner/OwnerController.java
+++ src/main/java/com/app/garage/controllers/Owner/OwnerController.java
@@ -86,10 +86,7 @@ public class OwnerController {
         viewPanel.getChildren().clear();
         viewPanel.getChildren().add(root);
         
-        //reloading th menupane to fix the overload issue.
-       // System.out.println("Reloading menuUI");
-      //  Parent menuUI = FXMLLoader.load(getClass().getResource("/UI/OwnerPage/Owner-form.fxml"));
-      //  menuPanel.getChildren().add(root);
+      
     }
     private void clearStyles(){
         labelHome.setStyle("");
