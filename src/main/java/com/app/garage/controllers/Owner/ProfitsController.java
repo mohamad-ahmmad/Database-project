ac8909203734a08860b2cf30d38af5ea1bb0a591
Mohammad Bdair <m.abed.bdair-@hotmail.com>
2022-07-01 20:06:28 +0300

    fixed

--- src/main/java/com/app/garage/controllers/Owner/ProfitsController.java
+++ src/main/java/com/app/garage/controllers/Owner/ProfitsController.java
@@ -3,13 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.app.garage.controllers.Owner;
-<<<<<<< HEAD
+
 import io.github.palexdev.materialfx.controls.MFXTableView;
-=======
 
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import java.io.IOException;
->>>>>>> 0f492e2969e648cc86a3e1ffbc48ba06aea211a4
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -26,7 +25,6 @@ import javafx.scene.chart.PieChart;
 public class ProfitsController implements Initializable  {
     @FXML
     private PieChart pieChart;
-<<<<<<< HEAD
      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -39,23 +37,6 @@ public class ProfitsController implements Initializable  {
             );
             pieChart.setTitle("Example Title");
             pieChart.setData(pieChartData);
-=======
-    @FXML
-    private MFXTableView profitTable;
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-        
-        
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-        new PieChart.Data("Sample 1" ,25 ),
-         new PieChart.Data("Sample 2" , 25),
-          new PieChart.Data("Sample 3", 25),
-           new PieChart.Data("Sample 4", 25)
-         );
-        pieChart.setTitle("Example Title");
-        pieChart.setData(pieChartData);
->>>>>>> 0f492e2969e648cc86a3e1ffbc48ba06aea211a4
     }
-    
+   
 }
